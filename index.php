ba4eeb0bc5d1bd9a6f6cfb80b24bb7dfa5a1dcf7
Macnolo <yosoymacnolo@gmail.com>
2019-05-12 16:18:39 -0500

    Agregando post de Autolocker

--- index.php
+++ index.php
@@ -98,12 +98,20 @@
             </div>
             <div class="text-justify">
                 <div class="card">
-            <h5 class="card-header">Nuevo</h5>
-            <div class="card-body">
-                <h5 class="card-title">Iniciando contenido...</h5>
-                <p class="card-text">Hola usuario! Esto es Macnolo Tech. Es todavia un pagina en proceso!</p>
-            </div>
-            </div>
+                    <h5 class="card-header">Destacado</h5>
+                    <div class="card-body">
+                        <h5 class="card-title">AutoLocker</h5>
+                        <p class="card-text">Conjunto de programas para volver a tu Arduino un sistema de seguridad para puertas.</p>
+                        <a href="https://autolocker.macnolo.tk" role="button" class="btn btn-link">Ver más</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <h5 class="card-header">Nuevo</h5>
+                    <div class="card-body">
+                        <h5 class="card-title">Iniciando contenido...</h5>
+                        <p class="card-text">Hola usuario! Esto es Macnolo Tech. Es todavia un pagina en proceso!</p>
+                    </div>
+                </div>
             </div>
         </div>
         <!-- iOS Repo Modal -->
