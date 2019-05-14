a64b28521a4a69274259d83c2e85b2124f55e0dd
Macnolo <yosoymacnolo@gmail.com>
2019-05-14 18:39:41 -0500

    Mejorando footer

--- index.php
+++ index.php
@@ -97,6 +97,13 @@
                 WIP - Trabajo en Progreso. Posibles errores!!
             </div>
             <div class="text-justify">
+				<div class="card">
+                    <h5 class="card-header">Nuevo</h5>
+                    <div class="card-body">
+                        <h5 class="card-title">Iniciando contenido...</h5>
+                        <p class="card-text">Hola usuario! Esto es Macnolo Tech. Es todavia un pagina en proceso!</p>
+                    </div>
+                </div>
                 <div class="card">
                     <h5 class="card-header">Destacado</h5>
                     <div class="card-body">
@@ -150,9 +157,11 @@
             </div>
         </div>
         <!-- Footer -->
-        <div class="container-fluid container-footer">
-            <p>Creado por Macnolo ©2019</p>
-        </div>
+		<footer class="footer mt-auto py-3">
+			<div class="container">
+				<p>Creado por Macnolo ©2019</p>
+			</div>
+		</footer>
         <!-- Scripting -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
