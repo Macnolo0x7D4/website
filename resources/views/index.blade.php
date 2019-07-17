4bac944c055d7a895d8139dc6e160df936fa08cf
Manuel Díaz <yosoymacnolo@gmail.com>
2019-07-17 12:22:36 -0500

    Tarjeta de Volcano

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,10 +5,12 @@
 </div>
 <div class="text-justify">
     <div class="card shadow-sm">
+      <img src="/img/volcano.png" class="card-img-top"  alt="">
         <h5 class="card-header">Nuevo</h5>
         <div class="card-body">
-            <h5 class="card-title">Iniciando contenido...</h5>
-            <p class="card-text">Hola usuario! Esto es Macnolo Tech. Es todavia un pagina en proceso!</p>
+            <h5 class="card-title">Conoce Volcano! El nuevo juego de Macnolo Tech</h5>
+            <p class="card-text">Hola usuario! Entra y prueba la nightly de Volcano!</p>
+            <a href="/volcano" role="button" class="btn btn-link">Jugar</a>
         </div>
     </div>
     <div class="card shadow-sm">
@@ -16,7 +18,7 @@
         <div class="card-body">
             <h5 class="card-title">AutoLocker</h5>
             <p class="card-text">Conjunto de programas para volver a tu Arduino un sistema de seguridad para puertas.</p>
-            <a href="https://macnolo.tk" role="button" class="btn btn-link">Ver más</a>
+            <a href="/autolocker" role="button" class="btn btn-link">Ver más</a>
         </div>
     </div>
     <div class="card shadow-sm">
