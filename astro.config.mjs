0eded80ebc09654a23f1b5746b018663947fafa6
Manuel Diaz <yosoymacnolo@gmail.com>
2024-05-12 13:43:41 -0600

    feat: add server side rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import node from "@astrojs/node";
 
 export default defineConfig({
-	site: 'https://macnolo.net',
-	integrations: [mdx(), sitemap()],
+  site: 'https://macnolo.net',
+  integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
