d946afd87f5cf1213c2a13280ccee5da95fc7d85
Manuel Díaz <yosoymacnolo@gmail.com>
2025-04-27 14:20:48 -0600

    refactor: restart project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import node from "@astrojs/node";
 
-export default defineConfig({
-  site: 'https://macnolo.net',
-  integrations: [mdx(), sitemap()],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
-});
+// https://astro.build/config
+export default defineConfig({});
